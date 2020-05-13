83b08d58576d4313ea5e23d8a969a334eb104853
Roman S <romjkeeeen@gmail.com>
2020-05-13 13:25:58 +0300

    REF articles

--- app/Http/Controllers/API/ArticleController.php
+++ app/Http/Controllers/API/ArticleController.php
@@ -3,23 +3,40 @@
 namespace App\Http\Controllers\API;
 
 use App\Article;
+use App\ArticleCategory;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+/**
+ * @group Articles
+ *
+ * APIs for
+ */
 class ArticleController extends Controller
 {
     /**
-     * @return \Illuminate\Http\JsonResponse
+     * List
+     * Список статей по категориям
+     *
+     * @authenticated required
+     * @response 200 {"data":[],"status": "success"}
      */
     public function index()
     {
-        $data = Article::all();
+        $data = ArticleCategory::with('articles')->paginate(15);
         return response()->json([
             'data' => $data,
             'status' => 'success'
         ], 200);
     }
 
+    /**
+     * Show
+     * Показать конкретную статью
+     *
+     * @authenticated required
+     * @response 200 {"data":{},"status":"success"}
+     */
     public function show($id)
     {
         $data = Article::where('id','=',$id)->get();
