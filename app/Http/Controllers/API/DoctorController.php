f2eec2a1a9f21b818a49c86a78d07dabec0f48e0
max <maxonilov@gmail.com>
2020-05-28 19:30:32 +0300

    api: doctors/:id add clinic.schedules

--- app/Http/Controllers/API/DoctorController.php
+++ app/Http/Controllers/API/DoctorController.php
@@ -62,6 +62,6 @@ class DoctorController extends Controller
      */
     public function show($id): DoctorRecourse
     {
-        return DoctorRecourse::make(Doctor::query()->with(['user', 'clinics', 'specialisations'])->findOrFail($id));
+        return DoctorRecourse::make(Doctor::query()->with(['user', 'clinics.schedules', 'specialisations'])->findOrFail($id));
     }
 }
--- app/Http/Resources/DoctorRecourse.php
+++ app/Http/Resources/DoctorRecourse.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Resources;
 
-use App\Clinic;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -16,17 +15,8 @@ class DoctorRecourse extends JsonResource
      */
     public function toArray($request)
     {
-        /** @var Clinic $clinic */
-        $clinic = $this->clinics->first();
-        if ($clinic) {
-            $clinic->addHidden(['schedules']);
-        }
-
         return collect(parent::toArray($request))
             ->except('clinics')
-            ->merge([
-                'clinic'    => $clinic,
-                'schedules' => $this->clinics->first()->schedules ?? [],
-            ])->toArray();
+            ->merge(['clinic' => $this->clinics->first()])->toArray();
     }
 }
