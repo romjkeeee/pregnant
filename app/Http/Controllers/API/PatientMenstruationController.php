8e22eead5b980878a30f7bb56f15b3c5904e1a84
Roman S <romjkeeeen@gmail.com>
2020-08-03 23:17:53 +0300

    ref menstruation

--- app/Http/Controllers/API/PatientMenstruationController.php
+++ app/Http/Controllers/API/PatientMenstruationController.php
@@ -32,19 +32,32 @@ class PatientMenstruationController extends Controller
     public function index()
     {
         $patient = Patient::query()->where('user_id', auth()->id())->first();
-        $menstruation = PatientMenstruation::query()->where('patient_id', $patient->id)->first();
-        $last_menstruation = new Carbon($menstruation->start_last_menstruation);
-        $next_menstruation_start = $last_menstruation->addDays($menstruation->duration_menstruation);
-        $next_menstruation_finish = $next_menstruation_start->addDays($menstruation->duration_cycle);
+        $menstruation = PatientMenstruation::query()->where('patient_id', $patient->id)->orderBy('created_at', 'desc')->first();
+        $last_menstruation = strtotime($menstruation->start_last_menstruation);
 
-        $start_ovulation = $next_menstruation_start->addDays(5);
-        $finis_ovulation = $next_menstruation_start->addDays(12);
+        $next_menstruation_start = strtotime('+'.$menstruation->duration_menstruation.' days', $last_menstruation);
+        $next_menstruation_finish = strtotime('+'.$menstruation->duration_cycle.' days', $next_menstruation_start);
+
+        $ovulation_data = $menstruation->duration_menstruation / 2 + 1;
+
+        $ovulation_middle = strtotime('+'.$ovulation_data.' days', $last_menstruation);
+        $start_ovulation = strtotime('-1 days', $ovulation_middle);
+        $finis_ovulation = strtotime('+1 days', $ovulation_middle);
+
+        $next_menstruation_start_second = strtotime('+'.$menstruation->duration_menstruation.' days', $next_menstruation_start);
+        $next_menstruation_finish_second = strtotime('+'.$menstruation->duration_cycle.' days', $next_menstruation_start_second);
+        $ovulation_middle_second = strtotime('+'.$ovulation_data.' days', $next_menstruation_start_second);
+        $start_ovulation_second = strtotime('-1 days', $ovulation_middle_second);
+        $finis_ovulation_second = strtotime('+1 days', $ovulation_middle_second);
+
+
 
         return \response([
-            'next_menstruation_start' => $next_menstruation_start->toDateString(),
-            'next_menstruation_finish' => $next_menstruation_finish->toDateString(),
-            'start_ovulation' => $start_ovulation->toDateString(),
-            'finish_ovulation' => $finis_ovulation->toDateString()
+            'next_menstruation_start' => [date('Y-m-d',$next_menstruation_start), date('Y-m-d',$next_menstruation_start_second)],
+            'next_menstruation_finish' => [date('Y-m-d',$next_menstruation_finish), date('Y-m-d',$next_menstruation_finish_second)],
+            'start_ovulation' => [date('Y-m-d',$start_ovulation), date('Y-m-d',$start_ovulation_second)],
+            'ovulation_date' => [date('Y-m-d',$ovulation_middle),date('Y-m-d',$ovulation_middle_second)],
+            'finish_ovulation' => [date('Y-m-d',$finis_ovulation), date('Y-m-d',$finis_ovulation_second)]
         ]);
     }
 
