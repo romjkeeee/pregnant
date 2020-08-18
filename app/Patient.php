e5612b52b02141a28103bc2e8853f295a6d8cacf
Roman S <romjkeeeen@gmail.com>
2020-08-18 12:57:03 +0300

    ref checklist

--- app/Patient.php
+++ app/Patient.php
@@ -84,6 +84,6 @@ class Patient extends BaseModel
 
     public function remember(): BelongsToMany
     {
-        return $this->belongsToMany(PatientTaskRemember::class, 'patient_task_remembers', 'patient_id', 'task_id');
+        return $this->belongsToMany(PatientTaskRemember::class, 'patient_task_remembers', 'task_id', 'patient_id');
     }
 }
