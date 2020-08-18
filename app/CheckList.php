39207b4151767ca246241e77ffe7dbbab81ebe3e
Roman S <romjkeeeen@gmail.com>
2020-08-18 13:18:25 +0300

    ref checklist

--- app/CheckList.php
+++ app/CheckList.php
@@ -50,6 +50,6 @@ class CheckList extends BaseModel
 
     public function remember(): BelongsToMany
     {
-        return $this->belongsToMany(CheckListTask::class, 'patient_task_remembers', 'task_id', 'id');
+        return $this->belongsToMany(CheckListTask::class, 'patient_task_remembers', 'list_id', 'id');
     }
 }
