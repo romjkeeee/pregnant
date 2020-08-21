f795fa23f4b9886314d8df2e75d12aff7a2094c7
Vladislav Zelenyk <vlad95715@gmail.com>
2020-08-21 13:52:56 +0300

    Fix search clinics

--- app/Http/Resources/ClinicResource.php
+++ app/Http/Resources/ClinicResource.php
@@ -16,13 +16,6 @@ class ClinicResource extends JsonResource
     public function toArray($request)
     {
         return array_merge(parent::toArray($request), [
-            'translates' => [
-                'lang_id'        => $this->translate->lang_id ?? null,
-                'clinic_id'        => $this->translate->lang_id ?? null,
-                'name'        => $this->translate->name ?? null,
-                'text'        => $this->translate->text ?? null,
-                'address'     => $this->translate->address ?? null,
-            ],
             'city'        => CityResource::make($this->city),
             'region'      => RegionResource::make($this->region),
             'prices'      => ClinicPriceResource::collection($this->prices),
