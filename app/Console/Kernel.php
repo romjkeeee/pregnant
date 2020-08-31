ec432e2797b37cc35d36b382dfc2254d38b6cac7
Roman S <romjkeeeen@gmail.com>
2020-08-31 22:55:28 +0300

    ref check list

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
          $schedule->command('patient:notify')
-                  ->dailyAt("9:00");
+                  ->cron('* * * * *');
     }
 
     /**
