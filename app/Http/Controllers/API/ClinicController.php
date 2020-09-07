6a1e158b925f322771a2dc44fa3cac53584d1d9f
Vladislav Zelenyk <vlad95715@gmail.com>
2020-09-07 14:28:12 +0300

    Add translates search clinics

--- app/Http/Controllers/API/ClinicController.php
+++ app/Http/Controllers/API/ClinicController.php
@@ -84,7 +84,7 @@ class ClinicController extends Controller
 
     public function search(Request $request)
     {
-            return Clinic::query()->with(['region.translates', 'city.translates', 'departments.translates', 'schedules', 'reviews', 'prices', 'translates'])
+            return Clinic::query()->with(['region.translates', 'city.translates', 'departments.translates', 'schedules', 'reviews', 'prices.translates', 'translates'])
                 ->when($request->get('type'), function (Builder $query) use ($request) {
                     $query->where('type', $request->get('type'));
                 })
