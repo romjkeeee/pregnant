e6b53f957764d6b2c8c7d79a697253fe44e539e7
Roman S <romjkeeeen@gmail.com>
2020-09-09 13:22:51 +0300

    ref

--- app/Console/Commands/NotifyTaskPatient.php
+++ app/Console/Commands/NotifyTaskPatient.php
@@ -47,7 +47,7 @@ class NotifyTaskPatient extends Command
         $users = PatientTaskRemember::query()->whereNotNull('date')->get();
         foreach($users as $user) {
             if ($user->remember == 1) {
-                if ($user->date == Carbon::now()->toDateString() ) {
+                if ($user->date >= Carbon::now()->toDateString() ) {
                     $patient = $user->patient;
                     $check_list = CheckListTaskTranslate::query()
                         ->where('task_id', $user->task_id)->first();
