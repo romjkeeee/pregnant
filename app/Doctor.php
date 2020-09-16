706f5647f668cccd492ee997e640f833d036bef6
zelvad <vlad95715@gmail.com>
2020-09-17 00:13:40 +0300

    add: like,dislike and fix

--- app/Doctor.php
+++ app/Doctor.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Doctor extends BaseModel
 {
-    protected $fillable = ['user_id', 'rate', 'experience'];
+    protected $fillable = ['user_id', 'rate', 'experience', 'like', 'dislike'];
 
     /**
      * @return HasOne
