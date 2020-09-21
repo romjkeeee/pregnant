e5eb4cc075f3e82aa4598a2131e9cfde21672f3d
Roman S <romjkeeeen@gmail.com>
2020-09-21 23:20:04 +0300

    ref command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('patient:notify')
+        $schedule->command(NotifyTaskPatient::class, ['--force'])->daily();
+
+        $schedule->command('patient:notify')
                   ->everyMinute();
     }
 
